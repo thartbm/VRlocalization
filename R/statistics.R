e42186e734675b87a92ec68e412d26388d22b5b1
Marius 't Hart <thartbm@gmail.com>
2024-07-28 13:44:26 -0400

    removed all data pre-processing functions from statistics script

--- R/statistics.R
+++ R/statistics.R
@@ -23,19 +23,32 @@ readFiles <- function(files, phases, trialtypes, baseline) {
     temp <- read.csv(file, stringsAsFactors = FALSE)
     temp <- prepareData(temp, file)
     if (is.data.frame(df)) {
-      df <- rbind(df, temp)
+      # this only happens for the pen files: write special function?
+      df <- combinePenData(df, temp)
+      # df <- rbind(df, temp)
     } else {
       df <- temp
     }
   }
   
+  if (length(files) > 1) {
+    df <- addDeviations(df)
+  }
   
-  print(files)
   if (baseline) {
     df <- applyBaseline(df)
   }
   
   return(df)
+  
+}
+
+combinePenData <- function(aligned, rotated) {
+  
+  # treat this as one continues task:
+  rotated$trialset_idx <- rotated$trialset_idx + max(aligned$trialset_idx)
+  
+  return(rbind(aligned, rotated))
   
 }
 
@@ -51,6 +64,12 @@ prepareData <- function(df, file) {
     # add unique participant ID:
     pre <- list('data/hand_30.csv'='h30', 'data/hand_60.csv'='h60')[[file]]
     df$unid <- sprintf('%s_%s', pre, df$ppid)
+    
+    # add condition name:
+    df$condition <- list('data/hand_30.csv'='hand30', 'data/hand_60.csv'='hand60')[[file]]
+    
+    # add effector:
+    df$effector <- 'hand'
   } else {
     # longer pen things, different way to prep contents:
     
@@ -63,19 +82,29 @@ prepareData <- function(df, file) {
     
     # remove break or instruction "trials" / empty lines
     df <- df[which(df$type != ''),]
+    
+    # add condition name:
+    df$condition <- 'pen30'
+    
+    # add effector
+    df$effector <- 'hand'
+    df$effector[which(df$pen_present)] <- 'pen'
   }
   
   # get block numbers / trial set numbers
-  df <- addBlockNumbers(df)
+  df <- addTrialSetNumbers(df)
   
   # add trial type:
   df <- addTrialType(df)
+  
+  # remove practice blocks:
+  df <- removePractice(df)
   
   return(df)
 
 }
 
-addBlockNumbers <- function(df) {
+addTrialSetNumbers <- function(df) {
   
   # get all trial numbers in a vector:
   trial_nums <- sort(unique(df$trial_num))
@@ -90,7 +119,7 @@ addBlockNumbers <- function(df) {
   }
   
   # apply map to add a column of block indices:
-  df$block_idx <- map[df$trial_num]
+  df$trialset_idx <- map[df$trial_num]
   
   return(df)
   
@@ -107,10 +136,81 @@ addTrialType <- function(df) {
   
 }
 
+removePractice <- function(df) {
+  
+  df <- df[which(df$block_num > 7),]
+    
+  return(df)
+  
+}
+
+addDeviations <- function(df) {
+  
+  for (trialtype in c('nocursor', 'localization')) {
+    
+    idx <- which(df$trialtype == trialtype)
+    
+    if (trialtype == 'nocursor') {
+      for (effector in c('pen','hand')) {
+        if (effector == 'hand') {
+          df$deviation[idx] <- df$cursor_final_angle[idx] - df$target_angle[idx]
+        }
+        if (effector == 'pen') {
+          df$deviation[idx] <- df$pen_final_angle[idx] - df$target_angle[idx]
+        }
+      }
+      
+    }
+    
+    if (trialtype == 'localization') {
+      df$deviation[idx] <- df$localizing_angle[idx] - df$arc_aquired_angle[idx]
+    }
+    
+  }
+  
+  return(df)
+  
+}
+
 applyBaseline <- function(df) {
   
-  print(class(df$trialtype))
-  print(unique(df$trialtype))
+  for (effector in unique(df$effector)) {
+    
+    for (trialtype in unique(df$trialtype[which(df$phase == 'aligned' & df$effector == effector)])) {
+      
+      print(c(effector, trialtype))
+      
+      
+      
+      AL_trialset_idx <- unique(df$trialset_idx[which(df$phase == 'aligned' & df$trialtype == trialtype)])
+      RO_trialset_idx <- unique(df$trialset_idx[which(df$phase == 'rotated' & df$trialtype == trialtype)])
+      
+      print(range(AL_trialset_idx))
+      print(range(RO_trialset_idx))
+      
+      targets <- unique(df$target_angle)
+      
+      for (pp in unique(df$unid)) {
+        
+        for (target in targets) {
+          
+          AL_idx <- which(df$unid == pp & df$target_angle == target & df$trialset_idx %in% AL_trialset_idx)
+          RO_idx <- which(df$unid == pp & df$target_angle == target & df$trialset_idx %in% RO_trialset_idx)
+          
+          baseline_bias <- median( df$deviation[AL_idx], na.rm=TRUE )
+          
+          df$deviation[RO_idx] <- df$deviation[RO_idx] - baseline_bias
+          
+        }
+        
+        
+      }
+      
+    }
+    
+  }
+  
+  return(df)
   
 }
 
